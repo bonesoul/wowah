4bb25340f65e792b5e03e995730f8f75c56c111e
Alexander Elo <kask@kask.fi>
2016-02-22 18:03:33 +0200

    Initial configuration for codeigniter

--- web/application/config/config.php
+++ web/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://kask.fi/wowah/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -314,7 +314,8 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+// TODO: Change on production upload
+$config['encryption_key'] = hex2bin('12a19294534e0977644f7cfa84d98a20');
 
 /*
 |--------------------------------------------------------------------------
---
+++ web/application/controllers/Front.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kask
+ * Date: 22.2.2016
+ * Time: 18.01
+ */
+
+class Front extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        echo 'Front ok';
+    }
+}
