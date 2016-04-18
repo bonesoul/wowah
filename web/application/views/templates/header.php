b10fc3203cd99a6f9504fe9714bbefa65374d53a
Alexander Elo <kask@kask.fi>
2016-04-18 14:41:01 +0300

    Added bunch of items

--- web/application/views/templates/header.php
+++ web/application/views/templates/header.php
@@ -53,14 +53,51 @@
                 </button>
                 <a class="navbar-brand" href="<?php echo site_url(); ?>">Ravencrest</a>
             </div>
+
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
                     <li><a href="<?php echo site_url(); ?>">Front</a></li>
+
                     <li><a href="#">Placeholder</a></li>
+
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Herbalism <span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Professions<span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="pricedata/herbalism">Daily price</a></li>
+                            <li class="dropdown-header">Daily prices</li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/herbalism">Herbalism</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/mining">Mining</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/gems">Jewelcrafting</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/glyphs">Inscription</a></li>
+
+                        </ul>
+                    </li>
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Trade goods<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-header">Daily prices</li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/archaeologyfragments">Archaeology Fragments</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/boeepics">BoE Epics</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/boerares">BoE Rares</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/consumablesenhancements">Consumables and Enhancements</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/containers">Containers</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/crafteditems">Crafted Items</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/darkmooncards">Darkmoon Cards</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/darkmoonquest">Darkmoon Faire Quest Items</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/questitems">Quest Items</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/miscarmortmogcloth">Misc, Armor & TMog: Cloth</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/miscarmortmogjwlshirtsetc">Misc, Armor & TMog: Jewelry, Shirts, OH, etc</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/miscarmortmogleather">Misc, Armor & TMog: Leather</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/miscarmortmogmail">Misc, Armor & TMog: Mail</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/miscarmortmogplate">Misc, Armor & TMog: Plate</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/miscarmortmogshields">Misc, Armor & TMog: Shields</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/miscarmortmogweapons">Misc, Armor & TMog: Weapons</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/droppedmountspets">Mounts & Pets: Dropped</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/craftedmountspets">Mounts & Pets: Crafted</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/recipes">Recipes</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/toys">Toys, Non-Crafted</a></li>
+                            <li><a href="<?php echo site_url(); ?>pricedata/othertradegoods">Other Trade Goods</a></li>
+
                         </ul>
                     </li>
                 </ul>
@@ -70,6 +107,7 @@
                     </div>
                 </form>
             </div>
+
         </div>
     </nav>
 
