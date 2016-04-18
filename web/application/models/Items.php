1740e31ba2a660b37e43c4d205932b1568f6437f
Alexander Elo <kask@kask.fi>
2016-04-18 13:55:44 +0300

    Get item name based on its ID

--- web/application/models/Items.php
+++ web/application/models/Items.php
@@ -25,4 +25,20 @@ class Items extends CI_Model
         // Return count of results
         return $ahDataDb->count_all_results();
     }
+
+    // Returns item name based on its ID
+    public function getItemName($itemId)
+    {
+        // Load correct database module
+        $ahDataDb = $this->load->database('ah_data', TRUE);
+
+        // Construct db query
+        $ahDataDb->select('name');
+        $ahDataDb->from('items');
+        $ahDataDb->where('itemId', $itemId);
+
+        // Return item name
+        $q = $ahDataDb->get();
+        return $q->result();
+    }
 }
