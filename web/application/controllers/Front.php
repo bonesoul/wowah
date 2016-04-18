304fdc60632bd3eae92bc25cca0f254903a27836
Alexander Elo <kask@kask.fi>
2016-04-18 13:14:35 +0300

    Merge db stat front and back end

--- web/application/controllers/Front.php
+++ web/application/controllers/Front.php
@@ -16,6 +16,25 @@ class Front extends CI_Controller
     public function index()
     {
         $this->load->view('templates/header');
+        $this->load->view('db_stats', $this->getDbStats());
         $this->load->view('templates/footer');
+    }
+
+    private function getDbStats()
+    {
+        $this->load->model('auctions');
+        $this->load->model('items');
+        $this->load->model('pulls');
+        $this->load->model('unknown');
+
+        $data = array(
+            'auctionCount' => $this->auctions->getAuctionCount(),
+            'itemCount' => $this->items->getItemCount(),
+            'queryCount' => $this->pulls->getPullCount(),
+            'lastQuery' => $this->pulls->getLatestUnixTime()->result()[0]->unixtime,
+            'unknownCount' => $this->unknown->getUnknownCount()
+        );
+
+        return $data;
     }
 }
