633f91c9a3ac8c80470b6d65bb446ab9a0577b5f
Alexander Elo <kask@kask.fi>
2016-04-18 13:59:13 +0300

    Link herbalism daily price nav item to correct URL

--- web/application/views/templates/header.php
+++ web/application/views/templates/header.php
@@ -60,7 +60,7 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Herbalism <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Daily price</a></li>
+                            <li><a href="pricedata/herbalism">Daily price</a></li>
                         </ul>
                     </li>
                 </ul>
